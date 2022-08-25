a0627ba2360cda7142305083cd3c41c7544931e9
benjaminaudet <benjamin@audet.fr>
2022-08-25 11:07:10 +0200

    Console implementation to make the user able to test it through CLI

--- main.php
+++ main.php
@@ -7,24 +7,47 @@
   require_once 'Classes/IsoscelesTriangle.php';
   require_once 'Classes/GeneralTriangle.php';
 
-  $annulus = new Annulus(5, 3);
-  $annulus->dump();
+  function createInstance($class, $params) {
+    $reflection_class = new ReflectionClass($class);
+    return $reflection_class->newInstanceArgs($params);
+  }
 
-  $disk = new Disk(5);
-  $disk->dump();
+  function displayHelp($commands) {
+    echo("This program is meant to be used to compute perimeters and areas of 2D geometrical shapes.".PHP_EOL);
+    echo("To use it, type a command as follow : shape_name args1 args2 args3".PHP_EOL);
+    echo("Availables geometrical shapes:".PHP_EOL);
+    foreach ($commands as $command) {
+      echo(sprintf("\"%s\" shape requires \"%d\" arguments to be created and computed.".PHP_EOL, $command[0], $command[2]));
+    }
+    echo("If you need more informations, please check : https://github.com/benjaminaudet/yet-another-shape-calculator/blob/main/README.md".PHP_EOL);
+  }
 
-  $square = new Square(5);
-  $square->dump();
+  $commands = [
+    ['annulus', Annulus::class, 2],
+    ['disk', Disk::class, 1],
+    ['square', Square::class, 1],
+    ['rectangle', Rectangle::class, 2],
+    ['right_triangle', RightTriangle::class, 2],
+    ['isosceles_triangle', IsoscelesTriangle::class, 2],
+    ['general_triangle', GeneralTriangle::class, 3],
+  ];
 
-  $rectangle = new Rectangle(5, 4);
-  $rectangle->dump();
+  while ($input = strtolower(readline())) {
+    foreach ($commands as $command) {
+      $args = explode(' ', $input);
+      if ($args[0] === $command[0]) {
+        if (count($args) < $command[2]) {
+          echo("The geometrical shape you're trying to create requires more arguments.".PHP_EOL);
+          break;
+        }
+        $obj = createInstance($command[1], array_slice($args, 1));
+        $obj->dump();
+        break;
+      } else if ($args[0] === "help") {
+        displayHelp($commands);
+        break;
+      }
+    }
+  }
 
-  $rightTriangle = new RightTriangle(12, 8);
-  $rightTriangle->dump();
-
-  $isoscelesTriangleShape = new IsoscelesTriangle(12, 8);
-  $isoscelesTriangleShape->dump();
-
-  $generalTriangleShape = new GeneralTriangle(12, 8, 5);
-  $generalTriangleShape->dump();
 ?>
